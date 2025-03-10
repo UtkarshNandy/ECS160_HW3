b829b6e55d15fab3f64c963182dffafbce3dc4f1
Utkarsh Nandy <Utkarsh.nandy@gmail.com>
2025-03-10 07:09:32 -0700

    Completed code for microservice b

--- HW3-Micro-B/src/main/java/org/example/HashtaggingController.java
+++ HW3-Micro-B/src/main/java/org/example/HashtaggingController.java
@@ -1,18 +1,39 @@
 package org.example;
 
+import io.github.ollama4j.exceptions.OllamaBaseException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import io.github.ollama4j.OllamaAPI;
+import io.github.ollama4j.models.response.OllamaResult;
+import io.github.ollama4j.types.OllamaModelType;
+import io.github.ollama4j.utils.OptionsBuilder;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 
 @RestController
 public class HashtaggingController {
 
     @PostMapping("/hashtag")
-    public String hashtag(@RequestBody MyRequest request){
+    public String hashtag(@RequestBody MyRequest request) throws OllamaBaseException, IOException, URISyntaxException, InterruptedException {
 
-        return "SECOND MICRO REACHED";
+        String content = request.getPostContent();
 
+        String host = "http://localhost:11434/";
+        OllamaAPI ollamaAPI = new OllamaAPI(host);
+        ollamaAPI.setRequestTimeoutSeconds(60);
+        String prompt = "Generate a one word hashtag for the following sentence: " + content;
+        OllamaResult result =
+                ollamaAPI.generate(OllamaModelType.LLAMA2, prompt, false, new OptionsBuilder().build());
+
+        return result.getResponse();
     }
+
+
 
     static class MyRequest {
         private String postContent;
--- HW3-Micro-B/src/main/java/org/example/HashtaggingService.java
+++ HW3-Micro-B/src/main/java/org/example/HashtaggingService.java
@@ -1,12 +1,26 @@
 package org.example;
+import io.github.ollama4j.OllamaAPI;
+import io.github.ollama4j.exceptions.OllamaBaseException;
+import io.github.ollama4j.models.generate.OllamaStreamHandler;
+import io.github.ollama4j.models.response.OllamaResult;
+import io.github.ollama4j.types.OllamaModelType;
+import io.github.ollama4j.utils.OptionsBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.Duration;
+import java.net.http.HttpClient;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 @SpringBootApplication
 public class HashtaggingService {
-    public static void main(String[] args) {
-        SpringApplication.run(HashtaggingService.class, args);
+    public static void main(String[] args) throws OllamaBaseException, IOException, URISyntaxException, InterruptedException {
+
+       SpringApplication.run(HashtaggingService.class, args);
     }
 }
